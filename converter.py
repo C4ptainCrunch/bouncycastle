97d273c3b84d234a29c14e232fab8835cb45ce4a
Nikita Marchant <nikita.marchant@gmail.com>
2013-04-01 16:59:48 +0200

    Doctrings, comments and readability

--- converter.py
+++ converter.py
@@ -7,35 +7,47 @@ import time
 import subprocess
 from subprocess import check_output
 
-def process_file(requestpath):
-    url = requestpath.text()
+def process_url(requestpath):
+    '''Processes a url into a mp3 and updates .status files.
+    Deletes .request at the end of the processing phase'''
+
     token = requestpath.namebase
-    print 'Converting {}'.format(url)
+    url = requestpath.text()
+
+    print 'Converting {}'.format(url) # Usefull to debug
+
     statusfile = PROCESSING_DIR/(token+'.status')
     statusfile.write_text('Wip')
-    audiopath = PROCESSING_DIR/(token+'.audio')
+
     try:
+        audiopath = PROCESSING_DIR/(token+'.audio')
         title = convert_file(url,audiopath)
-        print 'Done.'
+        print 'Done.' # Usefull to debug
         statusfile.write_text(title)
+
     except:
         print 'Error during convert.'
         statusfile.write_text('Error')
+
     finally:
-        requestpath.unlink()
+        requestpath.unlink() # Delete .request file
+
 
 def convert_file(url,path):
+    '''Converts a url to a mp3 into `path` and returns the song title'''
 
-    title = check_output(TITLE_CMD.format(url), shell=True).strip()
     check_output(CONVERT_CMD.format(path=path,url=url), shell=True)
-    return title
+
+    # Return the title of the song
+    return check_output(TITLE_CMD.format(url), shell=True).strip()
 
 try:
     while True :
         files = PROCESSING_DIR.files('*.request')
         if len(files) > 0:
-            process_file(files[0])
+            process_url(files[0])
         time.sleep(2)
+
 except KeyboardInterrupt:
-    print '\nThx, bye !\n'
+    print '\nMy work here is done. Goodbye old friend.\n'
 
